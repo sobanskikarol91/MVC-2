c0d191fb9b0fb7651e84bde2ef9c1621c3d961be
Karol Sobanski <sobanskikarol91@gmail.com>
2019-11-02 09:40:58 +0100

    Add: MVC (Tile, Slot, Board) + interfaces

---
+++ Match/Assets/Scripts/Slot/SlotController.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public interface ISlotController { } 
+
+public class SlotController : ISlotController
+{
+
+}
---
+++ Match/Assets/Scripts/Slot/SlotModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public interface ISlotModel { }
+
+public class SlotModel : ISlotModel
+{
+
+}
